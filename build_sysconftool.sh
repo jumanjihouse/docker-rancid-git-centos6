18fa34be7c51c05f693329e351e29611ec7f0dd1
Paul Morgan <jumanjiman@gmail.com>
2014-09-07 16:29:37 +0000

    use find command to locate spec files
    When I build on my coreos host locally, the spec file for sysconftool
    gets installed in `/rpmbuild/SPECS/`.
    
    When I build on wercker, the spec file for sysconftool
    gets installed in `/root/rpmbuild/SPECS/`.
    
    Therefore, use `find` command to locate the spec file
    and binary regardless of environment.

--- build_sysconftool.sh
+++ build_sysconftool.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 set -e
 
+srpm="sysconftool-0.17-1.fc20.src.rpm"
+
 cd /tmp/
-curl -L -O http://fedora.mirror.lstn.net//releases/20/Everything/source/SRPMS/s/sysconftool-0.17-1.fc20.src.rpm
-rpm -Uvh sysconftool-*.rpm
-yum-builddep -y /rpmbuild/SPECS/sysconftool.spec
-rpmbuild -ba /rpmbuild/SPECS/sysconftool.spec
-yum -y localinstall /rpmbuild/RPMS/noarch/sysconftool-*.rpm
+curl -L -O http://fedora.mirror.lstn.net/releases/20/Everything/source/SRPMS/s/$srpm
+rpm -Uvh /tmp/$srpm
+
+spec=$(find / -name sysconftool.spec)
+echo "spec: $spec"
+yum-builddep -y $spec
+rpmbuild -ba $spec
+
+rpms="$(find / -regex '.*sysconftool.*noarch.rpm')"
+echo "rpms: $rpms"
+yum -y localinstall $rpms
